801cf1e94d34f03dcd82205ebb0783e744be12df
misterwhy <weezixx@hotmail.com>
2020-12-25 17:40:06 +0100

    +5000 mots webscraper, il faut encore les mettre sous format : nl fr 0 0

--- test.py
+++ test.py
@@ -1,6 +1,36 @@
-nl =  [1,3,5,7]
-en =  [2,4,6,8]
+import sqlite3
 
-for i,j in zip(nl,en):
-    print(i)
-    print(j)
+connexion = sqlite3.connect("bdd.db")
+
+curseur = connexion.cursor()
+
+#SELECT * FROM words ORDER BY RANDOM() LIMIT 1
+
+curseur.execute("SELECT * FROM words WHERE trials = 0 ORDER BY RANDOM()  LIMIT 1")
+#curseur.execute("UPDATE words SET trials = 0 WHERE id = 5425 ")
+
+# rows = curseur.execute("SELECT * FROM words ")
+#
+rows = curseur.fetchall()
+
+for row in rows:
+
+     ligne = row[0]
+
+     reponse = input('quelle est la traduction de : '+ row[2])
+
+     reponse = reponse + "\n"
+
+     if reponse == row[1]:
+          print("gooooooooooood")
+
+     else:
+          print("raté la réponse était : " + row[1])
+
+#      curseur.execute("UPDATE words SET trials = 1 WHERE id = ? ",(ligne,))
+#
+# curseur.execute("SELECT * FROM words WHERE id = ?", (ligne,))
+#
+# rows = curseur.fetchall()
+#
+# print(rows)
